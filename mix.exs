16d64c79d8cea505d5a214c8c34c7eb8aa04ea88
chinponya <czen+github@honk.li>
2021-10-29 23:32:57 +0200

    version 0.0.2 -> 0.1.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Soulless.MixProject do
     [
       app: :soulless,
       name: "Soulless",
-      version: "0.0.2",
+      version: "0.1.0",
       elixir: "~> 1.12",
       description: "Mahjong Soul API client",
       source_url: "https://github.com/chinponya/soulless",
