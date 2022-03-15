ad9fae1bbca6d4b71252c5768212ed9bf9d9af5b
chinponya <czen+github@honk.li>
2022-03-15 17:26:10 +0100

    do not use global variables

--- priv/scripts/generate_proto_file.py
+++ priv/scripts/generate_proto_file.py
@@ -3,79 +3,115 @@
 import json
 import sys
 from collections import OrderedDict
-from io import StringIO
 from pathlib import Path
 from pprint import pprint
 
-input_file = Path(sys.argv[1])
-output_file = Path(sys.argv[2])
-data = json.load(input_file.open('r'), object_pairs_hook=OrderedDict)
-buf = StringIO()
-buf.write('syntax = "proto3";\n\n')
 
-data = data['nested']
-assert len(data) == 1
-package_name = list(data.keys())[0]
-buf.write('package {};\n\n'.format(package_name))
-data = data[package_name]
-data = data['nested']
-
-indent = 0
+def override_type(parent_name, field_name, current_type):
+    if parent_name != "Wrapper" and current_type == "bytes":
+        return "Wrapper"
+    elif parent_name == "Error" and field_name == "code":
+        return "ErrorCode"
+    else:
+        return current_type
 
 
-def write_line(line=''):
-    buf.write('{}{}\n'.format(' ' * 2 * indent, line))
+def format_line(line, indent):
+    return f"{' ' * 2 * indent}{line}\n"
 
 
-def parse_fields(fields):
-    for name in fields:
-        if 'rule' in fields[name]:
-            rule = fields[name]['rule'] + ' '
-        else:
-            rule = ''
-        write_line('{}{} {} = {};'.format(rule, fields[name]['type'], name, fields[name]['id']))
+def parse_rule(field):
+    if "rule" in field:
+        return field["rule"] + " "
+    else:
+        return ""
 
 
-def parse_methods(methods):
-    for name in methods:
-        write_line(
-            'rpc {} ({}) returns ({});'.format(name, methods[name]['requestType'], methods[name]['responseType']))
+def parse_fields(message, fields, indent):
+    return (
+        format_line(
+            f"{parse_rule(field)}{override_type(message, name, field['type'])} {name} = {field['id']};",
+            indent,
+        )
+        for name, field in fields.items()
+    )
 
 
-def parse_values(values):
-    for name in values:
-        write_line('{} = {};'.format(name, values[name]))
+def parse_methods(methods, indent):
+    return (
+        format_line(
+            f"rpc {name} ({method['requestType']}) returns ({method['responseType']});",
+            indent,
+        )
+        for name, method in methods.items()
+    )
 
 
-def parse_item(name, item):
-    global indent
-    if 'fields' in item:
-        write_line('message {} {{'.format(name))
-        indent += 1
-        parse_fields(item['fields'])
-    elif 'methods' in item:
-        write_line('service {} {{'.format(name))
-        indent += 1
-        parse_methods(item['methods'])
-    elif 'values' in item:
-        write_line('enum {} {{'.format(name))
-        indent += 1
-        parse_values(item['values'])
+def parse_values(values, indent):
+    return (format_line(f"{name} = {value};", indent) for name, value in values.items())
+
+
+def parse_item(name, item, indent):
+    lines = []
+
+    if "fields" in item:
+        lines = [
+            format_line(f"message {name} {{", indent),
+            *parse_fields(name, item["fields"], indent + 1),
+        ]
+    elif "methods" in item:
+        lines = [
+            format_line(f"service {name} {{", indent),
+            *parse_methods(item["methods"], indent + 1),
+        ]
+    elif "values" in item:
+        lines = [
+            format_line(f"enum {name} {{", indent),
+            *parse_values(item["values"], indent + 1),
+        ]
     else:
         pprint(item)
-        raise Exception('Unrecognized Item')
-    if 'nested' in item:
+        raise Exception("Unrecognized Item")
+
+    if "nested" in item:
         assert len(item) == 2
-        nested = item['nested']
-        for child in nested:
-            parse_item(child, nested[child])
+        for name, item in item["nested"].items():
+            lines.extend(parse_item(name, item, indent + 1))
 
-    indent -= 1
-    write_line('}\n')
+    lines.append(format_line("}\n", indent))
+
+    return lines
 
 
-for name in data:
-    parse_item(name, data[name])
+def parse(input_file):
+    data = json.load(input_file.open("r"), object_pairs_hook=OrderedDict)
+    data_nested = data["nested"]
+    assert len(data_nested) == 1
+    package_name = list(data_nested.keys())[0]
 
-with output_file.open('w') as f:
-    f.write(buf.getvalue())
+    lines = [
+        'syntax = "proto3";\n\n',
+        f"package {package_name};\n\n",
+        'import "error.proto";\n\n',
+    ]
+
+    package = data_nested[package_name]
+    package_nested = package["nested"]
+
+    for name, item in package_nested.items():
+        lines.extend(parse_item(name, item, 0))
+
+    return lines
+
+
+def main():
+    input_file = Path(sys.argv[1])
+    output_file = Path(sys.argv[2])
+    lines = parse(input_file)
+
+    with output_file.open("w") as f:
+        f.writelines(lines)
+
+
+if __name__ == "__main__":
+    main()
